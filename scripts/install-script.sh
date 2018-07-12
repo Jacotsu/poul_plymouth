8042489ee42aec5c4c5d09c720a815d1e8e4b62f
Jacotsu <Jacotsu@users.noreply.github.com>
2018-07-12 02:11:03 +0200

    Fixed typo

--- scripts/install-script.sh
+++ scripts/install-script.sh
@@ -2,7 +2,7 @@
 THEME_NAME='tema_plymouth_poul'
 
 # File copy installation
-sudo mkdir /usr/share/plymouth/themes/$haTHEME_NAME
+sudo mkdir /usr/share/plymouth/themes/$THEME_NAME
 sudo cp -r ../{img,$THEME_NAME*} /usr/share/plymouth/themes/$THEME_NAME
 sudo rm /usr/share/plymouth/themes/$THEME_NAME/img/*.svg
 sudo ln -sf /usr/share/plymouth/themes/$THEME_NAME/$THEME_NAME.plymouth /etc/alternatives/default.plymouth
