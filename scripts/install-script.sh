4cd665bd47f72b2121cdc20979245176f25609c8
Jacotsu <Jacotsu@users.noreply.github.com>
2018-08-12 18:12:09 +0200

    Started implementing boot messages log

--- scripts/install-script.sh
+++ scripts/install-script.sh
@@ -4,6 +4,9 @@ THEME_NAME='tema_plymouth_poul'
 # File copy installation
 sudo mkdir /usr/share/plymouth/themes/$THEME_NAME
 sudo cp -r ../{img,$THEME_NAME*} /usr/share/plymouth/themes/$THEME_NAME
+sudo cp journalctl-logger.sh /usr/bin
+sudo cp plymouth-journalctl.service /etc/systemd/system/
+sudo systemctl enable plymouth-journalctl.service
 sudo rm /usr/share/plymouth/themes/$THEME_NAME/img/*.svg
 sudo ln -sf /usr/share/plymouth/themes/$THEME_NAME/$THEME_NAME.plymouth /etc/alternatives/default.plymouth
 sudo ln -sf /usr/share/plymouth/themes/$THEME_NAME/$THEME_NAME.grub /etc/alternatives/default.plymouth.grub
---
+++ scripts/journalctl-logger.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env sh
+DEBUG=0
+
+decho(){
+    if [ "$DEBUG" = "1" ]; then
+        echo "$@"
+    fi
+}
+
+cursor=$(journalctl --output-fields=PRIORITY,MESSAGE --no-pager -b -0\
+    -o json | head -n 1 | grep -Po '(?<="__CURSOR"\ :\ ").*?[^\\](?=",)');
+
+while ! plymouth --ping; do
+
+    journalctl --output-fields=PRIORITY,MESSAGE --no-pager -b -0 -c "$cursor" \
+        -o json | while IFS= read -r line ;
+    do
+        cursor=$(echo "$line" | grep -Po \
+            '(?<="__CURSOR"\ :\ )".*?[^\\]"(?=,)');
+        priority=$(echo "$line" | grep -Po \
+            '(?<="PRIORITY"\ :\ )".*?[^\\]"(?=,)');
+        message=$(echo "$line" | grep -Po \
+            '(?<="MESSAGE"\ :\ )".*?[^\\]"(?=\ )');
+
+        case "$priority" in
+            *[0-3]*)
+                msg="colmsg:1:0.976:0.043:0.043:$message"
+                plymouth update --status="$msg"
+                ;;
+            *4*)
+                msg="colmsg:1:1:0.792:0:$message"
+                plymouth update --status="$msg"
+                ;;
+            *[5-7]*)
+                msg="colmsg:1:0.156:1:0:$message"
+                plymouth update --status="$msg"
+                ;;
+        esac
+
+        #decho "$cursor"
+        #decho "$priority"
+        #decho "$message"
+        #decho "$line"
+    done
+done
